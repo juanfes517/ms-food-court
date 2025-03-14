8b0ff43d852f834a18832f9abeaea4a49b7885cc
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-14 18:04:35 -0500

    feat: add service port for restaurant with save method

---
+++ src/main/java/com/pragma/foodcourt/domain/api/IRestaurantServicePort.java
@@ -0,0 +1,8 @@
+package com.pragma.foodcourt.domain.api;
+
+import com.pragma.foodcourt.domain.model.Restaurant;
+
+public interface IRestaurantServicePort {
+
+    Restaurant saveRestaurant(Restaurant restaurant);
+}
