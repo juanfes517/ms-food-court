491154d1ebb26c2d5b145019b1e54454758822ef
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-14 17:17:19 -0500

    first commit

---
+++ src/main/java/com/pragma/foodcourt/MsFoodCourtApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.foodcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFoodCourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFoodCourtApplication.class, args);
+	}
+
+}
