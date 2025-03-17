9b3f6ee09da912d6b5ab767b341f58b7708c4af0
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-17 11:08:36 -0500

    refactor: change the configuration of the ModelMapper bean

--- src/main/java/com/pragma/foodcourt/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/foodcourt/infrastructure/configuration/BeanConfiguration.java
@@ -1,10 +1,13 @@
 package com.pragma.foodcourt.infrastructure.configuration;
 
+import com.pragma.foodcourt.application.dto.request.RestaurantRequestDto;
 import com.pragma.foodcourt.domain.api.IRestaurantServicePort;
+import com.pragma.foodcourt.domain.model.Restaurant;
 import com.pragma.foodcourt.domain.spi.IRestaurantPersistencePort;
 import com.pragma.foodcourt.domain.spi.IUserExternalServicePort;
 import com.pragma.foodcourt.domain.usecase.RestaurantUseCase;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +21,16 @@ public class BeanConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addMappings(new PropertyMap<RestaurantRequestDto, Restaurant>() {
+            @Override
+            protected void configure() {
+                map().setOwnerId(source.getOwnerId());
+                skip(destination.getId());
+            }
+        });
+
+        return modelMapper;
     }
 }
