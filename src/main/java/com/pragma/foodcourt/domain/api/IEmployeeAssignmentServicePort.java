97b2caea62d91e5ea397952fff3847f274f18fdb
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-20 18:43:32 -0500

    refactor: change the return type of the method that assigns an employee to the restaurant

--- src/main/java/com/pragma/foodcourt/domain/api/IEmployeeAssignmentServicePort.java
+++ src/main/java/com/pragma/foodcourt/domain/api/IEmployeeAssignmentServicePort.java
@@ -1,8 +1,9 @@
 package com.pragma.foodcourt.domain.api;
 
+import com.pragma.foodcourt.domain.model.EmployeeAssignment;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface IEmployeeAssignmentServicePort {
 
-    Boolean assignEmployeeToRestaurant(Long employeeId, Long ownerId);
+    EmployeeAssignment assignEmployeeToRestaurant(Long employeeId, Long ownerId);
 }
