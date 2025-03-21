2685ea86687fe37aa91e6526b7431ef7e683ebde
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-21 15:01:18 -0500

    feat: add handler method that finds all restaurants

--- src/main/java/com/pragma/foodcourt/application/handler/IRestaurantHandler.java
+++ src/main/java/com/pragma/foodcourt/application/handler/IRestaurantHandler.java
@@ -1,9 +1,14 @@
 package com.pragma.foodcourt.application.handler;
 
 import com.pragma.foodcourt.application.dto.request.RestaurantRequestDto;
+import com.pragma.foodcourt.application.dto.response.RestaurantBasicInfoResponseDto;
 import com.pragma.foodcourt.application.dto.response.RestaurantResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IRestaurantHandler {
 
     RestaurantResponseDto saveRestaurant(RestaurantRequestDto restaurantRequestDto);
+
+    Page<RestaurantBasicInfoResponseDto> findAllRestaurants(Pageable pageable);
 }
--- src/main/java/com/pragma/foodcourt/application/handler/impl/RestaurantHandler.java
+++ src/main/java/com/pragma/foodcourt/application/handler/impl/RestaurantHandler.java
@@ -1,12 +1,15 @@
 package com.pragma.foodcourt.application.handler.impl;
 
 import com.pragma.foodcourt.application.dto.request.RestaurantRequestDto;
+import com.pragma.foodcourt.application.dto.response.RestaurantBasicInfoResponseDto;
 import com.pragma.foodcourt.application.dto.response.RestaurantResponseDto;
 import com.pragma.foodcourt.application.handler.IRestaurantHandler;
 import com.pragma.foodcourt.domain.api.IRestaurantServicePort;
 import com.pragma.foodcourt.domain.model.Restaurant;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,5 +27,11 @@ public class RestaurantHandler implements IRestaurantHandler {
         Restaurant savedRestaurant = restaurantUseCase.saveRestaurant(mappedRestaurant);
 
         return modelMapper.map(savedRestaurant, RestaurantResponseDto.class);
+    }
+
+    @Override
+    public Page<RestaurantBasicInfoResponseDto> findAllRestaurants(Pageable pageable) {
+        return restaurantUseCase.findAllRestaurants(pageable)
+                .map(restaurant -> modelMapper.map(restaurant, RestaurantBasicInfoResponseDto.class));
     }
 }
