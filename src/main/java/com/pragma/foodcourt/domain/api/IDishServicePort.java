b0482e4653c4771baf4727942071ff4f11ebec11
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-21 17:09:24 -0500

    feat: add use case method that finds all dishes

--- src/main/java/com/pragma/foodcourt/domain/api/IDishServicePort.java
+++ src/main/java/com/pragma/foodcourt/domain/api/IDishServicePort.java
@@ -1,6 +1,8 @@
 package com.pragma.foodcourt.domain.api;
 
 import com.pragma.foodcourt.domain.model.Dish;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IDishServicePort {
 
@@ -9,4 +11,6 @@ public interface IDishServicePort {
     Dish updateDish(Long dishId, Integer price, String description);
 
     Dish updateDishStatus(Long dishId, boolean status);
+
+    Page<Dish> findAllDishes(Pageable pageable, String categoryName, Long restaurantId);
 }
