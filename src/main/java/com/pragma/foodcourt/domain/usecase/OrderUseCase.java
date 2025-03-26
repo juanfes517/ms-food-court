f121be2e093a6d9a6fcda79ab7a9c3ec9d94a186
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-26 13:01:51 -0500

    feat: add handler method that finds all orders based on status and restaurant ID

--- src/main/java/com/pragma/foodcourt/domain/usecase/OrderUseCase.java
+++ src/main/java/com/pragma/foodcourt/domain/usecase/OrderUseCase.java
@@ -3,8 +3,10 @@ package com.pragma.foodcourt.domain.usecase;
 import com.pragma.foodcourt.domain.api.IOrderServicePort;
 import com.pragma.foodcourt.domain.exception.CustomerHasActiveOrderException;
 import com.pragma.foodcourt.domain.helper.constants.ExceptionConstants;
+import com.pragma.foodcourt.domain.model.EmployeeAssignment;
 import com.pragma.foodcourt.domain.model.Order;
 import com.pragma.foodcourt.domain.model.OrderStatusEnum;
+import com.pragma.foodcourt.domain.spi.IEmployeeAssignmentPersistencePort;
 import com.pragma.foodcourt.domain.spi.IJwtSecurityServicePort;
 import com.pragma.foodcourt.domain.spi.IOrderPersistencePort;
 import com.pragma.foodcourt.domain.spi.IUserExternalServicePort;
@@ -18,12 +20,34 @@ public class OrderUseCase implements IOrderServicePort {
     private final IUserExternalServicePort userExternalServicePort;
     private final IOrderPersistencePort orderPersistencePort;
     private final IJwtSecurityServicePort jwtSecurityServicePort;
+    private final IEmployeeAssignmentPersistencePort employeeAssignmentPersistencePort;
 
     @Override
     public Order placeOrder(Order order) {
         String tokenEmail = jwtSecurityServicePort.getSubject();
         Long customerId = userExternalServicePort.getUserIdByEmail(tokenEmail);
 
+        this.validateActiveOrder(customerId);
+        order.setCustomerId(customerId);
+
+        return orderPersistencePort.save(order);
+    }
+
+    @Override
+    public List<Order> getAllOrders(int page, int pageSize, OrderStatusEnum status) {
+        String tokenEmail = jwtSecurityServicePort.getSubject();
+        Long employeeId = userExternalServicePort.getUserIdByEmail(tokenEmail);
+        Long restaurantId = this.findEmployeeRestaurantId(employeeId);
+
+        return orderPersistencePort.findAll(page, pageSize, status, restaurantId);
+    }
+
+    private Long findEmployeeRestaurantId(Long employeeId) {
+        EmployeeAssignment employeeAssignment = employeeAssignmentPersistencePort.findByEmployeeId(employeeId);
+        return employeeAssignment.getRestaurant().getId();
+    }
+
+    private void validateActiveOrder(Long customerId) {
         List<Order> customerOrders = orderPersistencePort.findAllByCustomerId(customerId).stream()
                 .filter(customerOrder -> customerOrder.getStatus() == OrderStatusEnum.PREPARING
                         || customerOrder.getStatus() == OrderStatusEnum.PENDING
@@ -33,9 +57,5 @@ public class OrderUseCase implements IOrderServicePort {
         if (!customerOrders.isEmpty()) {
             throw new CustomerHasActiveOrderException(ExceptionConstants.CUSTOMER_HAS_ACTIVE_ORDER_EXCEPTION);
         }
-
-        order.setCustomerId(customerId);
-
-        return orderPersistencePort.save(order);
     }
 }
