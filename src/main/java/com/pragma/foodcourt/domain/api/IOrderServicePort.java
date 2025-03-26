69a3af248f439dcd809b4ad0dfa30ffaa5753d14
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-26 13:06:59 -0500

    feat: add use case method that finds all orders based on status and restaurant ID

--- src/main/java/com/pragma/foodcourt/domain/api/IOrderServicePort.java
+++ src/main/java/com/pragma/foodcourt/domain/api/IOrderServicePort.java
@@ -1,8 +1,13 @@
 package com.pragma.foodcourt.domain.api;
 
 import com.pragma.foodcourt.domain.model.Order;
+import com.pragma.foodcourt.domain.model.OrderStatusEnum;
+
+import java.util.List;
 
 public interface IOrderServicePort {
 
     Order placeOrder(Order order);
+
+    List<Order> getAllOrders(int page, int pageSize, OrderStatusEnum status);
 }
