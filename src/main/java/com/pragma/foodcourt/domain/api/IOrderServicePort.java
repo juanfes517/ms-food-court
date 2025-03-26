941149dff47fb4a0b4cf8950d13e76ee705aad77
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-26 13:03:15 -0500

    Revert "feat: add handler method that finds all orders based on status and restaurant ID"
    This reverts commit f121be2e093a6d9a6fcda79ab7a9c3ec9d94a186.

--- src/main/java/com/pragma/foodcourt/domain/api/IOrderServicePort.java
+++ src/main/java/com/pragma/foodcourt/domain/api/IOrderServicePort.java
@@ -1,13 +1,8 @@
 package com.pragma.foodcourt.domain.api;
 
 import com.pragma.foodcourt.domain.model.Order;
-import com.pragma.foodcourt.domain.model.OrderStatusEnum;
-
-import java.util.List;
 
 public interface IOrderServicePort {
 
     Order placeOrder(Order order);
-
-    List<Order> getAllOrders(int page, int pageSize, OrderStatusEnum status);
 }
