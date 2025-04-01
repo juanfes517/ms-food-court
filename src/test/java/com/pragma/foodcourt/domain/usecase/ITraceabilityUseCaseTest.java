0bcff4e3786ad41d5eff31e313640979587aee58
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-31 19:28:38 -0500

    test: add unit test for the use case method that gets a traceability of an order

---
+++ src/test/java/com/pragma/foodcourt/domain/usecase/ITraceabilityUseCaseTest.java
@@ -0,0 +1,59 @@
+package com.pragma.foodcourt.domain.usecase;
+
+import com.pragma.foodcourt.domain.model.Traceability;
+import com.pragma.foodcourt.domain.spi.ITraceabilityExternalService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ITraceabilityUseCaseTest {
+
+    @InjectMocks
+    private ITraceabilityUseCase traceabilityUseCase;
+
+    @Mock
+    private ITraceabilityExternalService traceabilityExternalService;
+
+    @Test
+    void getOrderTraceability() {
+        Long orderId = 1L;
+
+        Traceability traceability = Traceability.builder()
+                .orderId(1L)
+                .customerId(1L)
+                .customerEmail("customer@mail.com")
+                .date(LocalDateTime.now())
+                .previousStatus("PENDING")
+                .newStatus("PREPARING")
+                .employeeId(2L)
+                .employeeEmail("employee1@mail.com")
+                .build();
+
+        List<Traceability> traceabilityList = List.of(traceability);
+
+        when(traceabilityExternalService.getOrderTraceability(orderId))
+                .thenReturn(traceabilityList);
+
+        List<Traceability> result = traceabilityUseCase.getOrderTraceability(orderId);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(traceability.getOrderId(), result.get(0).getOrderId());
+        assertEquals(traceability.getCustomerId(), result.get(0).getCustomerId());
+        assertEquals(traceability.getCustomerEmail(), result.get(0).getCustomerEmail());
+        assertEquals(traceability.getDate(), result.get(0).getDate());
+        assertEquals(traceability.getPreviousStatus(), result.get(0).getPreviousStatus());
+        assertEquals(traceability.getNewStatus(), result.get(0).getNewStatus());
+        assertEquals(traceability.getEmployeeId(), result.get(0).getEmployeeId());
+        assertEquals(traceability.getEmployeeEmail(), result.get(0).getEmployeeEmail());
+    }
+}
