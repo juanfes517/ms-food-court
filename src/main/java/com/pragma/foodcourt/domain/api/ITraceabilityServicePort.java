5c74918349f3f56c85d8b05216d3ba5a1eecd3d4
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-31 19:28:23 -0500

    feat: add use case method that gets a traceability of an order

---
+++ src/main/java/com/pragma/foodcourt/domain/api/ITraceabilityServicePort.java
@@ -0,0 +1,10 @@
+package com.pragma.foodcourt.domain.api;
+
+import com.pragma.foodcourt.domain.model.Traceability;
+
+import java.util.List;
+
+public interface ITraceabilityServicePort {
+
+    List<Traceability> getOrderTraceability(Long orderId);
+}
---
+++ src/main/java/com/pragma/foodcourt/domain/usecase/ITraceabilityUseCase.java
@@ -0,0 +1,19 @@
+package com.pragma.foodcourt.domain.usecase;
+
+import com.pragma.foodcourt.domain.api.ITraceabilityServicePort;
+import com.pragma.foodcourt.domain.model.Traceability;
+import com.pragma.foodcourt.domain.spi.ITraceabilityExternalService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ITraceabilityUseCase implements ITraceabilityServicePort {
+
+    private final ITraceabilityExternalService traceabilityExternalService;
+
+    @Override
+    public List<Traceability> getOrderTraceability(Long orderId) {
+        return traceabilityExternalService.getOrderTraceability(orderId);
+    }
+}
