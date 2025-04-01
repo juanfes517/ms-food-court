63a0df6766762bd2029a0e744d38ff4fbad96633
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-31 22:42:06 -0500

    test: add unit test for the feign method that gets the EmployeeEfficiency

--- src/test/java/com/pragma/foodcourt/infrastructure/out/feign/adapter/TraceabilityFeignAdapterTest.java
+++ src/test/java/com/pragma/foodcourt/infrastructure/out/feign/adapter/TraceabilityFeignAdapterTest.java
@@ -1,5 +1,6 @@
 package com.pragma.foodcourt.infrastructure.out.feign.adapter;
 
+import com.pragma.foodcourt.domain.model.EmployeeEfficiency;
 import com.pragma.foodcourt.domain.model.RestaurantEfficiency;
 import com.pragma.foodcourt.domain.model.Traceability;
 import com.pragma.foodcourt.infrastructure.out.feign.client.TraceabilityFeignClient;
@@ -87,5 +88,38 @@ class TraceabilityFeignAdapterTest {
         assertEquals(restaurantEfficiency1.getFinalStatus(), result.get(0).getFinalStatus());
         assertEquals(restaurantEfficiency2.getOrderId(), result.get(1).getOrderId());
         assertEquals(restaurantEfficiency2.getFinalStatus(), result.get(1).getFinalStatus());
+    }
+
+    @Test
+    void getEmployeeEfficiency_WhenIsSuccessful() {
+        List<Long> employeeIds = List.of(101L, 102L);
+
+        EmployeeEfficiency efficiency1 = EmployeeEfficiency.builder()
+                .employeeId(employeeIds.get(0))
+                .employeeEmail("employee1@example.com")
+                .averageProcessingTimeInSeconds(120.5)
+                .build();
+
+        EmployeeEfficiency efficiency2 = EmployeeEfficiency.builder()
+                .employeeId(employeeIds.get(1))
+                .employeeEmail("employee2@example.com")
+                .averageProcessingTimeInSeconds(95.0)
+                .build();
+
+        List<EmployeeEfficiency> employeeEfficiencies = List.of(efficiency1, efficiency2);
+
+        when(traceabilityFeignClient.getEmployeeEfficiency(employeeIds))
+                .thenReturn(employeeEfficiencies);
+
+        List<EmployeeEfficiency> result = traceabilityFeignAdapter.getEmployeeEfficiency(employeeIds);
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(efficiency1.getEmployeeId(), result.get(0).getEmployeeId());
+        assertEquals(efficiency1.getEmployeeEmail(), result.get(0).getEmployeeEmail());
+        assertEquals(efficiency1.getAverageProcessingTimeInSeconds(), result.get(0).getAverageProcessingTimeInSeconds());
+        assertEquals(efficiency2.getEmployeeId(), result.get(1).getEmployeeId());
+        assertEquals(efficiency2.getEmployeeEmail(), result.get(1).getEmployeeEmail());
+        assertEquals(efficiency2.getAverageProcessingTimeInSeconds(), result.get(1).getAverageProcessingTimeInSeconds());
     }
 }
