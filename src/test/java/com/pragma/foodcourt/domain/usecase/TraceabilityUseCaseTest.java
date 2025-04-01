e313cde34d4105bb8b794fe1d1621d43dfa17e16
juanfes517 <juan.escobar15@udea.edu.co>
2025-03-31 21:29:24 -0500

    test: add unit test for the use case method that gets a restaurant efficiency

--- src/test/java/com/pragma/foodcourt/domain/usecase/TraceabilityUseCaseTest.java
+++ src/test/java/com/pragma/foodcourt/domain/usecase/TraceabilityUseCaseTest.java
@@ -1,7 +1,10 @@
 package com.pragma.foodcourt.domain.usecase;
 
+import com.pragma.foodcourt.domain.model.Order;
+import com.pragma.foodcourt.domain.model.Restaurant;
+import com.pragma.foodcourt.domain.model.RestaurantEfficiency;
 import com.pragma.foodcourt.domain.model.Traceability;
-import com.pragma.foodcourt.domain.spi.ITraceabilityExternalService;
+import com.pragma.foodcourt.domain.spi.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,8 +26,20 @@ class TraceabilityUseCaseTest {
     @Mock
     private ITraceabilityExternalService traceabilityExternalService;
 
+    @Mock
+    private IUserExternalServicePort userExternalService;
+
+    @Mock
+    private IJwtSecurityServicePort jwtSecurityService;
+
+    @Mock
+    private IRestaurantPersistencePort restaurantPersistencePort;
+
+    @Mock
+    private IOrderPersistencePort orderPersistencePort;
+
     @Test
-    void getOrderTraceability() {
+    void getOrderTraceability_WhenIsSuccessful() {
         Long orderId = 1L;
 
         Traceability traceability = Traceability.builder()
@@ -55,5 +70,60 @@ class TraceabilityUseCaseTest {
         assertEquals(traceability.getNewStatus(), result.get(0).getNewStatus());
         assertEquals(traceability.getEmployeeId(), result.get(0).getEmployeeId());
         assertEquals(traceability.getEmployeeEmail(), result.get(0).getEmployeeEmail());
+    }
+
+    @Test
+    void getRestaurantEfficiency_WhenIsSuccessful() {
+        String tokenEmail = "owner@mail.com";
+        Long ownerId = 1L;
+
+        Restaurant restaurant = Restaurant.builder()
+                .id(1L)
+                .build();
+
+        Order order1 = Order.builder()
+                .id(1L)
+                .build();
+
+        Order order2 = Order.builder()
+                .id(2L)
+                .build();
+
+        List<Order> orderList = List.of(order1, order2);
+        List<Long> orderIds = List.of(order1.getId(), order2.getId());
+
+        RestaurantEfficiency restaurantEfficiency1 = RestaurantEfficiency.builder()
+                .orderId(1L)
+                .finalStatus("status")
+                .orderDurationInSeconds(32)
+                .build();
+
+        RestaurantEfficiency restaurantEfficiency2 = RestaurantEfficiency.builder()
+                .orderId(2L)
+                .finalStatus("status")
+                .orderDurationInSeconds(23)
+                .build();
+
+        List<RestaurantEfficiency> restaurantEfficiencies = List.of(restaurantEfficiency1, restaurantEfficiency2);
+
+        when(jwtSecurityService.getSubject())
+                .thenReturn(tokenEmail);
+        when(userExternalService.getUserIdByEmail(tokenEmail))
+                .thenReturn(ownerId);
+        when(restaurantPersistencePort.findByOwnerId(ownerId))
+                .thenReturn(restaurant);
+        when(orderPersistencePort.findAllByRestaurantId(restaurant.getId()))
+                .thenReturn(orderList);
+        when(traceabilityExternalService.getRestaurantEfficiency(orderIds))
+                .thenReturn(restaurantEfficiencies);
+
+        List<RestaurantEfficiency> result = traceabilityUseCase.getRestaurantEfficiency();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(restaurantEfficiency1.getOrderId(), result.get(0).getOrderId());
+        assertEquals(restaurantEfficiency2.getOrderId(), result.get(1).getOrderId());
+        assertEquals(restaurantEfficiency1.getFinalStatus(), result.get(0).getFinalStatus());
+        assertEquals(restaurantEfficiency2.getFinalStatus(), result.get(1).getFinalStatus());
     }
 }
